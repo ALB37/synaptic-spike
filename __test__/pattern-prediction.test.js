652ba4c56a03787480a4f02da316b3e6d03559f2
Andrew Bloom <blooma116@gmail.com>
2018-02-06 23:43:30 -0800

    adding more tests and a real world wav file and parser to test

--- __test__/pattern-prediction.test.js
+++ __test__/pattern-prediction.test.js
@@ -1,13 +1,70 @@
 'use strict';
 
+const waveParser = require('../lib/sound-data-parser');
+const fs = require('fs-extra');
 const predictor = require('../lib/pattern-prediction');
 
-describe('testing that the xor network was correctly trained to perform the xor comparison', () => {
-  test('xor should return expected results', () => {
+describe('testing that the pattern prediction network was correctly trained to perform the pattern prediction comparison', () => {
+  test('pattern predictor should return expected results', () => {
     const oneOrZeroArr = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
     const countToOneArr = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+    const triangle = [
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+      1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 
+    ];
     expect(Math.round(predictor(oneOrZeroArr)[0])).toEqual(1);
     expect(Math.round((predictor(countToOneArr)[0]) * 10) / 10).toEqual(1);
+    expect(Math.round((predictor(triangle)[0]) * 10) / 10).toEqual(0.7);
+    const highResTri = [];
+    while (highResTri.length < 960){
+      let count = 0;
+      while (count < 0.99){
+        highResTri.push(count);
+        count += 0.01;
+      }
+      while (count > 0.01){
+        highResTri.push(count);
+        count -= 0.01;
+      }
+    }
+    highResTri.forEach((e, i, a) => {
+      a[i] = e + (Math.random() * 0.002);
+    });
+    expect(Math.round((predictor(highResTri)[0]) * 10) / 10).toEqual(0);
 
+    return fs.readFile(`${__dirname}/assets/one_hundred_ms_sin.wav`)
+      .then(file => {
+        let parsedFile = waveParser(file);
+        expect(Math.round((predictor(parsedFile.neuralArray)[0]) * 10 ) / 10).toEqual(0.5);
+      }); 
   });
 });
